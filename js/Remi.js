b753216bd66f5d339b4c518362905a5fb694122a
mastro.elfo <francesco.209@gmail.com>
2014-11-28 14:34:52 +0100

    Update

--- js/Remi.js
+++ js/Remi.js
@@ -41,8 +41,8 @@ Remi.prototype._init = function ()
  */
 Remi.prototype._loadLists = function () {
 	// Read localStorage at key 'lists'
-	$.Storage.load('lists', []);
-	return $.Storage.get('lists');
+	//$.Storage.load('lists', []);
+	return $.Storage.get('lists') || [];
 }
 
 /**
@@ -53,6 +53,9 @@ Remi.prototype._reloadDrawer = function () {
 	var lists=this._loadLists();
 	$.Dom.id('index-drawer-lists').innerHTML = '';
 	$.Each(lists, function(list, i){
+		if(!list) {
+			return;
+		}
 		$.Dom.inject($.Dom.element('li', {}, list.name, {
 			'click': function (event){
 				self.loadList(i);
@@ -68,7 +71,27 @@ Remi.prototype._reloadMasterSettings=function(){
 	var lists=this._loadLists();
 	$.Dom.id('settings-master-elements').innerHTML = '';
 	$.Each(lists, function(list, i){
-		$.Dom.inject($.Dom.element('li', {}, '<input type="text" value="'+list.name+'" data-index="'+i+'"/>'), $.Dom.id('settings-master-elements'));
+		var li=$.Dom.element('li', {});
+		var list_new_name=$.Dom.element('input', {
+			'type':'text',
+			'value':list.name,
+			'data-index':i,
+			'class': 'fit six'
+		});
+		var label = $.Dom.element('label', {
+			'class': 'pack-checkbox danger fit'
+		});
+		var delete_list=$.Dom.element('input', {
+			'type':'checkbox',
+			'data-index':i
+		});
+		var span = $.Dom.element('span');
+		$.Dom.inject(list_new_name, li);
+		$.Dom.inject(label, li)
+		$.Dom.inject(delete_list, label);
+		$.Dom.inject(span, label);
+		$.Dom.inject(li, $.Dom.id('settings-master-elements'));
+		
 	});
 }
 
@@ -82,11 +105,15 @@ Remi.prototype._reloadMasterSettings=function(){
  */
 Remi.prototype.createList = function (new_list_name) {
 	var lists=this._loadLists();
+	//alert('start create list '+lists+' lists.lenght= '+lists.length);
 	var found=null; // After the following Each is non-null if a list with the given name exists; null otherwise
 	
 	//alert(lists.length)
-	if (lists) { // TODO: eliminare questo if, lists non dovrebbe mai essere null
+	if (lists && lists.length) { // TODO: eliminare questo if, lists non dovrebbe mai essere null
 		$.Each(lists, function(list, i){
+			if (!list) {
+				return true;
+			}
 			if(list.name==new_list_name){ //TODO: uppercase/lowercase-trim-multiple blanks
 				found=i;
 				return false;
@@ -96,7 +123,7 @@ Remi.prototype.createList = function (new_list_name) {
 				return true;
 			}
 		});
-		//alert(found)
+		//alert('found='+found)
 	}
 	else {
 		lists = [];
@@ -123,11 +150,12 @@ Remi.prototype.createList = function (new_list_name) {
 				'alphabetical-order': false
 			}
 		});
+		//alert(lists);
 		$.Storage.set('lists', lists);
 		$.Storage.set('list-'+ uid, []);
 		
 	}
-	
+	//alert('here found:'+(found)+' length:'+(lists.length)+' l-1:'+(lists.length-1));
 	this.showList(found || lists.length -1);
 }
 
@@ -137,50 +165,56 @@ Remi.prototype.createList = function (new_list_name) {
  * return: null
  */
 Remi.prototype.showList = function (index){
-	// Load the list and its option
-	var lists = this._loadLists();
-	if (!lists) {
-		return;
-	}
-	
-	var list = lists[index];
-	var items = $.Storage.get('list-'+list.identifier);
+	/*emergency cleanup code*/
+	//localStorage['lists'] = null;
+	//return;
+	//$.Storage.set('lists', []);
+	//return;
 	
 	// Clear dom
+	$.Dom.id('index-listname').innerHTML = '';
 	$.Dom.id('index-itemslist-notchecked').innerHTML = '';
 	$.Dom.id('index-itemslist-checked').innerHTML = '';
-	if (list.options['move-to-bottom']) {
-		// Add items to dom with move-to-bottom option
-		$.Each(items, function(item){
-		//TODO: aggiungere bottone spunta
-		//TODO: aggiungere evento click
-			if (item.checked) {
-				$.Dom.inject($.Dom.element('li', {}, item.name), $.Dom.id('index-itemslist-checked'));
-			}
-			else{
-				$.Dom.inject($.Dom.element('li', {}, item.name), $.Dom.id('index-itemslist-notchecked'));
-			}
-		});
+	
+	// Load the list and its option
+	var lists = this._loadLists();
+	var list = lists[index];
+	
+	if (lists.length && list) {
+		var items = $.Storage.get('list-'+list.identifier);
+		
+		if (list.options['move-to-bottom']) {
+			// Add items to dom with move-to-bottom option
+			$.Each(items, function(item){
+			//TODO: aggiungere bottone spunta
+			//TODO: aggiungere evento click
+				if (item.checked) {
+					$.Dom.inject($.Dom.element('li', {}, item.name), $.Dom.id('index-itemslist-checked'));
+				}
+				else{
+					$.Dom.inject($.Dom.element('li', {}, item.name), $.Dom.id('index-itemslist-notchecked'));
+				}
+			});
+		}
+		else{
+			// Add items to dom as in localStorage order
+			$.Each(items, function(item){
+				if (item.checked) {
+					$.Dom.inject($.Dom.element('li', {'class':'checked'}, item.name), $.Dom.id('index-itemslist-notchecked'));
+				}
+				else{
+					$.Dom.inject($.Dom.element('li', {}, item.name), $.Dom.id('index-itemslist-notchecked'));
+				}
+			});
+		}
+		// Print list name
+		$.Dom.id('index-listname').innerHTML = list.name;
+		// Close drawer
+		location.href = '#';
 	}
-	else{
-		// Add items to dom as in localStorage order
-		$.Each(items, function(item){
-			if (item.checked) {
-				$.Dom.inject($.Dom.element('li', {'class':'checked'}, item.name), $.Dom.id('index-itemslist-notchecked'));
-			}
-			else{
-				$.Dom.inject($.Dom.element('li', {}, item.name), $.Dom.id('index-itemslist-notchecked'));
-			}
-		});
-	}
-	// Print list name
-	$.Dom.id('index-listname').innerHTML = list.name;
-	// Close drawer
-	location.href = '#';
 	
 	// Update lists position
 	this.updateListsPositions(index);
-
 }
 
 /**
@@ -193,18 +227,23 @@ Remi.prototype.updateListsPositions = function(index){
 	var orderedList = [];
 	var lists = this._loadLists();
 	
-	// Put last shown list in first place
-	orderedList[0] = lists[index];
-	
-	// Put all others lists in same order
-	$.Each(lists, function(list, key){
-		if(index != key) {
-			orderedList.push(list);
-		}
-	});
-	
-	// Save lists on localStorage
-	$.Storage.set('lists', orderedList);
+	if (lists && lists.length) {
+		// Put last shown list in first place
+		orderedList[0] = lists[index];
+		
+		// Put all others lists in same order
+		$.Each(lists, function(list, key){
+			if (!list) {
+				return;
+			}
+			if(index != key) {
+				orderedList.push(list);
+			}
+		});
+		
+		// Save lists on localStorage
+		$.Storage.set('lists', orderedList);
+	}
 	
 	this._reloadDrawer();
 }
@@ -219,19 +258,40 @@ Remi.prototype.loadList = function(index){
 }
 
 /**
- *
- *
+ * Edit lists names, new names are taken from Input
+ * return: this
  */
 Remi.prototype.editListNames=function(){
 	var lists=this._loadLists();
 	
-	$.Each($.Dom.select('#settings-master-elements input'), function(item){
-		lists[item.getAttribute('data-index')].name=item.value;
+	$.Each(lists, function(item, key){
+		item.name=$.Dom.select('#settings-master-elements input[data-index="'+key+'"]')[0].value;
 	});
 	
 	$.Storage.set('lists', lists);
 	
-/*4. Il sistema aggiorna le modifiche effettuate
- *1raccogliere input dal contenitore
- *2per ogni input leggo il valore e */
+	return this;
+}
+
+/**
+ * Delete marked lists
+ * return: this
+ */
+Remi.prototype.deleteLists=function(){
+	
+	var lists=this._loadLists();
+	var newlist=[];
+	
+	$.Each(lists, function(item, key){
+		if ($.Dom.select('#settings-master-elements input[type="checkbox"][data-index="'+key+'"]')[0].checked) {
+			$.Storage.set('list-'+item.identifier, []);
+		}
+		else{
+			newlist.push(item);
+		}
+	});
+	
+	$.Storage.set('lists', newlist);
+	
+	return this;
 }
